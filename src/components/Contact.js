0c16b2ed6aee444862a7196ac8271e51f6fefd64
B.Thomas <begue.thomas@laposte.net>
2020-12-18 17:33:34 +0100

    Modification des pages Contact et Accueil.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,15 @@ import React from 'react';
 const Contact = () => {
     return (
        <div>
-          <h1>Contact US</h1>
-          <p>Contact US page body content</p>
+          <h1>Contact</h1>
+           <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at rutrum odio. 
+            Nulla luctus risus enim, at ullamcorper orci commodo nec. Nulla pretium risus in iaculis consequat. 
+            Curabitur accumsan tellus est, sit amet elementum ante posuere at. Sed vitae metus tempus, porta velit vitae, gravida purus. 
+            Suspendisse sit amet odio et mauris convallis porttitor. Sed sed eleifend ex. Nullam semper elementum tortor tincidunt tincidunt. 
+            Praesent consequat scelerisque erat et imperdiet. Donec semper, nisl non tempus pulvinar, orci erat mollis risus, sed maximus odio eros nec nulla. 
+            Phasellus nunc enim, congue in ultrices nec, tincidunt eu nisl. Donec blandit cursus pellentesque. Nam tristique tellus iaculis volutpat elementum. 
+           </p>
        </div>
     );
 }
