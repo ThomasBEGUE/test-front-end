2f8a50a60d30b675722eb43be07b7c1c488a0736
B.Thomas <begue.thomas@laposte.net>
2020-12-19 12:07:03 +0100

    Modification de ListLanguages.

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -24,47 +24,30 @@ function ListLinks(props)
     );
 }
 
-function ListItemLanguage(props)
-{
-    if (props.selected) {
-        return null;
-    } 
-    return (
-        <a className="dropdown-item" href="#">{props.value}</a>
-    );
-}
-
-function ListItemSelectedLanguage(props)
-{
-    if (!props.selected) {
-        return null;
-    } 
-    return (
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            {props.value}
-        </a>
-    );
-}
-
 function ListLanguage(props)
 {
     const languages = props.languages;
-    const unselectedLangItems = languages.map((lang) => 
-        <ListItemLanguage key={lang.code} value={lang.name} selected={lang.selected} />
-    );
-    const selectedLangItem = languages.map((lang) => 
-        <ListItemSelectedLanguage key={lang.code} value={lang.name} selected={lang.selected} />
-    );
+    const [items] = React.useState(languages);
+
+    // Find the selected value
+    const selectedValue = items.filter(lang => {
+        return lang.selected === true;
+    });
+
+    // Controlling the dropdown value with state and set the initial value
+    const [value, setValue] = React.useState(selectedValue[0].code);
 
     return (
-        <ul className="navbar-nav my-2 my-lg-0">
-            <li className="nav-item dropdown">
-                {selectedLangItem}
-                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    {unselectedLangItems}
-                </div>
-            </li>
-        </ul>
+        <div className="input-group col-sm-3 col-md-3">
+            <select className="custom-select" id="inputGroupSelect03">
+                { items.map( ({name, code}) => (
+                    <option 
+                        key={code}
+                        value={code}
+                        onChange={e => setValue(e.currentTarget.value)}>{name}</option>
+                )) }
+            </select>
+        </div>
     );
 }
 
@@ -83,20 +66,11 @@ function NavigationBar ()
         <div className="container">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ListLinks titles={titles} />
-
-            <div class="input-group col-sm-3 col-md-3">
-                <select class="custom-select" id="inputGroupSelect03">
-                    <option value="fr_FR" selected>Français</option>
-                    <option value="en_GB">Anglais</option>
-                </select>
-            </div>
-
+            <ListLanguage languages={languages} />
           </div>
         </div>
       </nav>
     );
 }
-
-//             <ListLanguage languages={languages} />
 
 export default NavigationBar;
